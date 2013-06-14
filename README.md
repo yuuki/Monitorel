b56720f545fbc7d695def2b039687293fe02a101
y_uuki <yuki.tsubo@gmail.com>
2013-06-14 23:07:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Monitorel - It's new $module
+
+# SYNOPSIS
+
+    use Monitorel;
+
+# DESCRIPTION
+
+Monitorel is ...
+
+# LICENSE
+
+Copyright (C) y\_uuki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+y\_uuki <yuki.tsubo@gmail.com>
---
+++ lib/Monitorel.pm
@@ -0,0 +1,36 @@
+package Monitorel;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Monitorel - It's new $module
+
+=head1 SYNOPSIS
+
+    use Monitorel;
+
+=head1 DESCRIPTION
+
+Monitorel is ...
+
+=head1 LICENSE
+
+Copyright (C) y_uuki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+y_uuki E<lt>yuki.tsubo@gmail.comE<gt>
+
