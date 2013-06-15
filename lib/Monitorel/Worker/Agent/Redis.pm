65aa7cb490836eaaf77c67bf9346746b0afc058f
y_uuki <yuki.tsubo@gmail.com>
2013-06-15 12:36:55 +0900

    Refactor Redis

--- lib/Monitorel/Worker/Agent/Redis.pm
+++ lib/Monitorel/Worker/Agent/Redis.pm
@@ -7,36 +7,30 @@ use parent qw(Monitorel::Worker);
 use Carp qw(croak);
 use IO::Socket::INET;
 
-use constant REDIS_DEFAULT_PORT => 6379;
-
-our $STAT_NAMES = [ qw(
-    total_commands_processed
-    connected_clients
-    connected_slaves
-    changes_since_last_save
-    mem_fragmentation_ratio
-    used_memory_rss
-    expired_keys
-    evicted_keys
-    keyspace_hits
-    keyspace_misses
-    keys
-) ];
+use constant {
+    DEFAULT_PORT => 6379,
+    STAT_NAMES => [qw(
+        total_commands_processed
+        connected_clients
+        connected_slaves
+        changes_since_last_save
+        mem_fragmentation_ratio
+        used_memory_rss
+        expired_keys
+        evicted_keys
+        keyspace_hits
+        keyspace_misses
+        keys
+    )]
+};
 
 
 sub proc {
     my ($class, $args) = @_;
 
-    my $hostname = $args->{host}  // croak "host key must not be empty";
-    my $stats    = $args->{stats} // croak "stats key must not be empty";
-    my $port     = $args->{port}  || REDIS_DEFAULT_PORT;
-
-    my $all_stat_to_value = _redis_stat_to_value($hostname, $port);
-    +{ map { $_ => $all_stat_to_value->{$_} } @$stats };
-}
-
-sub _redis_stat_to_value {
-    my ($hostname, $port) = @_;
+    my $hostname = $args->{host}  or croak "host required";
+    my $stats    = $args->{stats} or croak "stats required";
+    my $port     = $args->{port}  || DEFAULT_PORT;
 
     my $sock = IO::Socket::INET->new(
         PeerAddr => "$hostname:$port",
@@ -46,27 +40,28 @@ sub _redis_stat_to_value {
 
     $sock->print("INFO\r\n");
 
-    my $item = {};
-    $item->{keys} = 0;
+    my $stat_to_value = {};
+    $stat_to_value->{keys} = 0;
     while (my $line = $sock->getline) {
         last if $line eq "\r\n";
         $line =~ s/\n$|\r\n$//;  #chomp
 
-        my ($k, $v) = split ":", $line;
-        next if ! defined $k or ! defined $v;
+        my ($stat, $value) = split ":", $line;
+        next if ! defined $stat or ! defined $value;
 
         if ($line =~ /^db\d+:keys=(\d+),/) {
-            $item->{keys} += $1;
+            $stat_to_value->{keys} += $1;
         } else {
-            $item->{$k} = _value_to_int($v);
+            $stat_to_value->{$stat} = value_to_int($value);
         }
     }
 
     $sock->close;
-    $item;
+
+    return +{ map { $_ => $stat_to_value->{$_} } @$stats };
 }
 
-sub _value_to_int {
+sub value_to_int {
     my $value = shift;
 
     if ($value =~ /^(\d+)$/) {
