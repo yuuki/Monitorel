ae6ab6feaceff8ba3ff40551b0942cacc6243ae0
y_uuki <yuki.tsubo@gmail.com>
2013-06-15 11:19:43 +0900

    Refactor Munin

--- lib/Monitorel/Worker/Agent/Munin.pm
+++ lib/Monitorel/Worker/Agent/Munin.pm
@@ -1,27 +1,22 @@
 package Monitorel::Worker::Agent::Munin;
 use strict;
 use warnings;
-use utf8;
 use parent qw(Monitorel::Worker);
 
 use Carp qw(croak);
 use IO::Socket;
-use constant BUFF_SIZE => 20000;
 
-our $DEFAULT_PORT = 4949;
+use constant {
+    BUFF_SIZE    => 20000,
+    DEFAULT_PORT => 4949,
+};
 
 sub proc {
     my ($class, $args) = @_;
 
-    my $hostname = $args->{host}  // croak "host key must not be empty";
-    my $services = $args->{stats};
-    my $port     = $args->{port}  || $DEFAULT_PORT;
-
-    _munin_service_to_stat_to_value($hostname, $port);
-}
-
-sub _munin_service_to_stat_to_value {
-    my ($hostname, $port) = @_;
+    my $hostname = $args->{host}  || croak "host requried";
+    my $services = $args->{stats} || croak "stats required";
+    my $port     = $args->{port}  || DEFAULT_PORT;
 
     my $sock = IO::Socket::INET->new(
         PeerAddr => $hostname,
@@ -30,41 +25,31 @@ sub _munin_service_to_stat_to_value {
         Timeout  => 10,
     ) || croak 'Cannot Connect';
 
-    my $avail_services = _avail_services($sock);
-    my $buffer = _munin_response($sock, $avail_services);
-
-    $sock->send("quit\n");
-    $sock->close;
-
-    _hashify_response($avail_services, $buffer);
-}
-
-sub _avail_services {
-    my $sock = shift;
-
     my $buff;
     $sock->recv($buff, BUFF_SIZE);
     $sock->send("list\n");
     $sock->recv($buff, BUFF_SIZE);
-    [ split(' ', $buff) ];
-}
+    my $avail_services = [ split(' ', $buff) ];
 
 # sendしたコマンドに対する応答をなぜか得られないため，
 # (前回のsendコマンドに対する応答を取得してしまったりする)
 # 一旦すべてのserviceに対する応答をテキストとして取得する
-sub _munin_response {
-    my ($sock, $services) = @_;
-
     my @buff = map {
         my $buff;
         $sock->send("fetch $_\n");
         $sock->recv($buff, BUFF_SIZE);
         $buff;
-    } @$services;
-    join("", @buff);
+    } @$avail_services;
+    my $buffer = join("", @buff);
+
+    $sock->send("quit\n");
+    $sock->close;
+
+    return hashify_response($avail_services, $buffer);
 }
 
-sub _hashify_response {
+
+sub hashify_response {
     my ($avail_services, $buffer) = @_;
 
     my $i = 0;
@@ -82,7 +67,7 @@ sub _hashify_response {
         $service_to_stat_to_value->{$serv}->{$stat} = $value;
     }
 
-    $service_to_stat_to_value;
+    return $service_to_stat_to_value;
 }
 
 1;
